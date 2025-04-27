7608be7e4e5bda461f9826ef22db5ee4ae2c864d
omarmeotaz-work <omarmoetaz1997@gmail.com>
2025-04-27 16:21:30 +0300

    edits

--- src/UI/Header.jsx
+++ src/UI/Header.jsx
@@ -6,8 +6,7 @@ function Header() {
   return (
     <header className="flex items-center justify-between bg-yellow-500 px-4 py-3 border-b border-stone-200 uppercase sm:px-6">
       <Link to="/" className="tracking-widest">
-        {" "}
-        Fast React Pizza Co.
+        Vite React Pizza Co.
       </Link>
       <SearchOrder />
 
